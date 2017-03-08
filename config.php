466b662f5fd8b21ac94179f1a0add43ad8a9b3e8
Jeroen Vriezema <jeroen.vriezema@gmail.com>
2017-03-08 13:01:14 +0100

    Updated config.php
    Added commentary

--- config.php
+++ config.php
@@ -1,14 +1,21 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-define("MYSQL_HOST", "localhost");
-define("MYSQL_USER", "vqx0zlpe_radiovergelijker");
-define("MYSQL_PASS", "Hjhqmw3g");
-define("MYSQL_DB", "vqx0zlpe_radiovergelijker");
-define("MYSQL_CHARSET", "utf8");
-setlocale(LC_ALL, 'nl_NL');
-define("BASE_URL", "https://radiovergelijker.nl");
-date_default_timezone_set("Europe/Amsterdam");
-
+<?php
+//Enable or disable PHP error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+//Define MYSQL Host
+define("MYSQL_HOST", "localhost");
+//Define MySQL user
+define("MYSQL_USER", "YOURUSER");
+//Define MySQL Password
+define("MYSQL_PASS", "YOURPW");
+//Define MySQL Database
+define("MYSQL_DB", "YOURDB");
+//Define MySQL character set
+define("MYSQL_CHARSET", "utf8");
+//Set locale for time formatting
+setlocale(LC_ALL, 'nl_NL');
+//define the base url
+define("BASE_URL", "https://radiovergelijker.nl");
+//Define the servers timezome
+date_default_timezone_set("Europe/Amsterdam");
 ?>
